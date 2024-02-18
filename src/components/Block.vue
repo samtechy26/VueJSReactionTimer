5d433ca7300da43a94fb093407a6828b4bc39a71
Sammy <samakan061@gmail.com>
2024-02-18 11:15:21 +0300

    second commit

---
+++ src/components/Block.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+
+        console.log(this.delay)
+        setTimeout(()=>{this.showBlock = true}, this.delay)
+        this.startTimer()
+    },
+
+    methods: {
+
+        startTimer () {
+
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+
+        }, 
+
+        stopTimer () {
+
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+
+        }
+    }
+    
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px;
+}
+</style>
